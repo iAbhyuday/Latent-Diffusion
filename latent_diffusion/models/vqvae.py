bf24f5a0b0a5e83b14952807c1dcaf0768835ad6
iabhyuday <abhyuday964808@gmail.com>
2024-09-20 15:49:00 +0000

    add: decoder blocks

--- latent_diffusion/models/vqvae.py
+++ latent_diffusion/models/vqvae.py
@@ -12,10 +12,10 @@ class VQVAE(nn.Module):
         self.codebook_size = codebook_size
         self.embed_dim = embed_dim
         self.use_ema = use_ema
-        self.encoder = Encoder(3)
-        self.pre_quant = nn.LazyConv2d(embed_dim, kernel_size=3, padding=1)
+        self.encoder = Encoder(resolution=32, num_resblock=2, ch_mult=[1, 4, 8],attn_resolution=[4, 8],ch_factor=32, out_channels=4)
+        self.pre_quant = nn.Conv2d(4, embed_dim, kernel_size=3, padding=1)
         self.vq = Quantizer(self.codebook_size, self.embed_dim, use_ema=self.use_ema)
-        self.decoder = Decoder(2, 3)
+        self.decoder = Decoder(in_channels=embed_dim, resolution=4, out_channels=3, ch_factor=1, ch_mult=[1, 4, 8], num_resblock=2, attn_resolution=[8, 16])
         self.percept_loss = PerceptualLoss(layers=[1, 6, 11, 20, 29], normalized=False)
 
     
@@ -28,4 +28,3 @@ class VQVAE(nn.Module):
         percept_loss = self.percept_loss(x_, input_image)
 
         return x_, code, commitment_loss, codebook_loss, recon_loss, percept_loss, encoding
-    
--- latent_diffusion/modules/decoder.py
+++ latent_diffusion/modules/decoder.py
@@ -1,29 +1,85 @@
 import torch as pt
 from torch import nn
 from .resblock import ResBlock
+from .attention import AttnBlock
+
+
+class Upsample(nn.Module):
+
+    def __init__(self, in_channels, out_channels=None, scale_factor: int = 2):
+        super(Upsample, self).__init__()
+        self.in_channels = in_channels
+        if out_channels is None:
+            out_channels = in_channels
+        self.out_channels = out_channels
+        self.scale = scale_factor
+
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.UpsamplingNearest2d(scale_factor=self.scale),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
 
 class Decoder(nn.Module):
     def __init__(
-            self,
-            in_channels: int,
-            out_channels: int
+        self,
+        in_channels: int,
+        out_channels: int,
+        ch_factor: int,
+        ch_mult=[1, 2, 4, 8],
+        num_resblock: int = 3,
+        attn_resolution=[],
+        resolution=4,
+        mid_block: bool = False,
     ):
         super(Decoder, self).__init__()
-        self.layers = nn.Sequential(
-            ResBlock(in_channels, 32),
-            ResBlock(32, 32),
-            nn.ConvTranspose2d(32, 64, 2, stride=2, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            ResBlock(64, 64),
-            ResBlock(64, 128),
-            nn.ConvTranspose2d(128, 128, 2, stride=2, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            ResBlock(128, 64),
-            ResBlock(64, 32),
-            nn.ConvTranspose2d(32, out_channels, 2, stride=2),
-        )
-    
-    def forward(self, x): 
-        return self.layers(x)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.ch_mult = ch_mult
+        self.num_resblock = num_resblock
+        self.num_ch_mult = len(ch_mult)
+        self.attn_resolution = attn_resolution
+        block_in = ch_factor * self.ch_mult[-1]
+        current_resolution = resolution // 2 ** (self.num_ch_mult - 1)
+        self.in_conv = nn.Conv2d(in_channels, block_in, kernel_size=3, padding=1)
+
+        self.layers = nn.ModuleDict()
+        for i in reversed(range(self.num_ch_mult)):
+            block_out = ch_factor * ch_mult[i]
+            upblock = nn.ModuleDict()
+            block = nn.ModuleDict()
+
+            for j in range(self.num_resblock):
+                block.add_module(f"resblock_{i}_{j}", ResBlock(block_in, block_out))
+                block_in = block_out
+            upblock.add_module(f"resblock_{i}", block)
+            if current_resolution in self.attn_resolution:
+                upblock.add_module(f"attn_{i}", AttnBlock(block_in))
+            if i != 0:
+                upblock.add_module(f"upsample_{i}", Upsample(block_in))
+                current_resolution = current_resolution * 2
+            self.layers.add_module(f"UpBlock_{i}", upblock)
+
+        if not mid_block:
+            self.layers.add_module("OutNorm", nn.BatchNorm2d(block_in))
+            self.layers.add_module(
+                "OutConv", nn.Conv2d(block_in, out_channels, kernel_size=3, padding=1)
+            )
+
+    def forward(self, x: pt.Tensor):
+        h = self.in_conv(x)
+        for i in reversed(range(self.num_ch_mult)):
+            for j in range(self.num_resblock):
+                h = self.layers[f"UpBlock_{i}"][f"resblock_{i}"][f"resblock_{i}_{j}"](h)
+            if f"attnblock_{i}" in self.layers[f"UpBlock_{i}"].keys():
+                _, h = self.layers[f"UpBlock_{i}"][f"attnblock_{i}"](h)
+            if f"upsample_{i}" in self.layers[f"UpBlock_{i}"].keys():
+                h = self.layers[f"UpBlock_{i}"][f"upsample_{i}"](h)
+        h = self.layers["OutNorm"](h)
+        h = self.layers["OutConv"](h)
+
+        return h
