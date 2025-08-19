ea9c27eb9431807def1123668b3b1bc092574ac9
Abhyuday Tripathi <abhyuday964808@gmail.com>
2025-08-20 00:21:01 +0530

    lightning api

--- train_cifar.py
+++ train_cifar.py
@@ -1,157 +1,81 @@
-# %%
 import os
 import yaml
 import torch
 import numpy as np
 from tqdm import tqdm
-from torch import Tensor, nn
+from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from torchvision.transforms import (
-    Normalize,
-    Resize,
-    ToTensor,
-    Compose,
-)
+from torchvision.transforms import Normalize, Resize, ToTensor, Compose
 from torchvision.datasets import CIFAR10
 from latent_diffusion.models import VQVAE
 from latent_diffusion.modules import PerceptualLoss
 from latent_diffusion.utils.metrics import measure_perplexity
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
 
-# %%
 with open("configs/cifar10.yaml", encoding="utf-8") as f:
     cfg = yaml.safe_load(f)
 trainer_cfg = cfg["trainer"]
-# %%
-torch.set_printoptions(precision=3, sci_mode=False)
-device = torch.device(trainer_cfg["device"])
-writer = SummaryWriter(
-    log_dir=os.path.join(trainer_cfg["tensorboard_log_dir"], trainer_cfg["name"])
+
+t_transforms = Compose([Resize(32), ToTensor(), Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+v_transforms = Compose([Resize(32), ToTensor(), Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+
+class CIFARDataModule(pl.LightningDataModule):
+    def __init__(self, train_batch_size, val_batch_size):
+        super().__init__()
+        self.train_batch_size = train_batch_size
+        self.val_batch_size = val_batch_size
+
+    def setup(self, stage=None):
+        self.train_data = CIFAR10(root="data", train=True, transform=t_transforms, download=True)
+        self.val_data = CIFAR10(root="data", train=False, transform=v_transforms, download=True)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_data, batch_size=self.train_batch_size, shuffle=True, num_workers=4, drop_last=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_data, batch_size=self.val_batch_size, shuffle=False, num_workers=4)
+
+
+data_module = CIFARDataModule(
+    train_batch_size=trainer_cfg["train_batch_size"],
+    val_batch_size=trainer_cfg["val_batch_size"]
 )
-# %%
-t_transforms = Compose(
-    [Resize(32), ToTensor(), Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]
+
+logger = TensorBoardLogger(
+    save_dir=trainer_cfg["tensorboard_log_dir"],
+    name=trainer_cfg["name"]
 )
+class ImageReconstructionCallback(pl.callbacks.Callback):
+    def __init__(self, num_images=8):
+        super().__init__()
+        self.num_images = num_images
 
-v_transforms = Compose(
-    [Resize(32), ToTensor(), Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]
-)
-train_data = CIFAR10(root="data", train=True, transform=t_transforms, download=True)
-data_var = np.var(train_data.data)
-print(data_var)
-val_data = CIFAR10(root="data", train=False, transform=v_transforms, download=True)
-# %%
-train_data = DataLoader(
-    dataset=train_data,
-    batch_size=trainer_cfg["trainw_batch_size"],
-    shuffle=True,
-    num_workers=4,
-    drop_last=True,
-)
-val_data = DataLoader(
-    dataset=val_data,
-    batch_size=trainer_cfg["val_batch_size"],
-    shuffle=True,
-    num_workers=4,
-)
-# %%
-model = VQVAE(cfg)
-# %%
-lr = float(trainer_cfg["lr"])
-n_epochs = 20
-num_batches = len(train_data)
-
-percept_loss = PerceptualLoss(**cfg["perceptual_loss"]).to(trainer_cfg["device"])
-optim = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-4)
-
-
-if cfg["trainer"]["load_ckpt"]:
-    checkpoint = torch.load(cfg["trainer"]["load_ckpt"], weights_only=True)
-    model.load_state_dict(checkpoint["model_state_dict"])
-
-
-best = 10
-for e in range(0, n_epochs):
-    r_recon = 0.0
-    r_com = 0.0
-    r_cdl = 0.0
-    r_pl = 0.0
-    r_ppl = 0.0
-
-    with tqdm(
-        train_data, unit="batch", desc=f"Epoch {e+1}", position=0, leave=True
-    ) as data:
-        model.train()
-        for batch_idx, (x, _) in enumerate(data):
-            writer.add_scalar(
-                "lr", optim.param_groups[0]["lr"], e * num_batches + batch_idx
-            )
-            x = x.to(device)
-
-            x_, cd, cl, cdl, rl, enc = model(x)
-            pl = percept_loss(x_, x)
-            loss = cl + rl + pl
-
-            r_recon += rl.item()
-            r_com += cl.item()
-            r_pl += pl.item()
-            ppl, clp = measure_perplexity(
-                enc, cfg["quantizer"]["params"]["codebook_size"]
-            )
-            r_ppl += ppl.item()
-            if cfg["quantizer"]["type"] == "vanilla":
-                loss += cdl
-                r_cdl += cdl.item()
-
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-
-            data.set_postfix(
-                {
-                    "recon_loss": r_recon / (batch_idx + 1),
-                    "commit_loss": r_com / (batch_idx + 1),
-                    "codebook_loss": r_cdl / (batch_idx + 1),
-                    "percept_loss": r_pl / (batch_idx + 1),
-                    "perplexity": r_ppl / (batch_idx + 1),
-                }
-            )
-
-        writer.add_scalar("recon_loss", r_recon / num_batches, e)
-        writer.add_scalar("commit_loss", r_com / num_batches, e)
-        writer.add_scalar("codebook_loss", r_cdl / num_batches, e)
-        writer.add_scalar("perceptual_loss", r_pl / num_batches, e)
-        writer.add_scalar("perplexity", r_ppl / num_batches, e)
-
-    rec_loss = (r_recon + r_pl) / num_batches
-    # sched.step(r_recon/num_batches)
-    if rec_loss < best:
-        best = rec_loss
-        checkpoint = {
-            "epoch": e,
-            "lr": optim.param_groups[0]["lr"],
-            "model_state_dict": model.state_dict(),
-            "optimizer_state_dict": optim.state_dict(),
-        }
-
-        torch.save(checkpoint, f"{trainer_cfg['name']}_best.pth")
-
-    if e % 1 == 0:
+    def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0):
+        images, _ = batch
+        images = images[:self.num_images]
         with torch.no_grad():
-            model.eval()
-            x, _ = next(iter(val_data))
-            x = x.to(device)
-            x_, cd, cl, cdl, rl, _ = model(x)
-            writer.add_images("input", x, e)
-            writer.add_images("output", x_, e)
+            reconstructions = pl_module(images.to(pl_module.device))
+        # If model returns tuple (recon, ...), take first
+        if isinstance(reconstructions, tuple):
+            reconstructions = reconstructions[0]
+        # Denormalize for visualization
+        def denorm(x):
+            return x * 0.5 + 0.5
+        img_grid = torch.cat([denorm(images.cpu()), denorm(reconstructions.cpu())], dim=0)
+        trainer.logger.experiment.add_images(
+            "Validation/Reconstruction",
+            img_grid,
+            global_step=trainer.global_step
+        )
 
-
-# %%
-checkpoint = {
-    "epoch": n_epochs,
-    "model_state_dict": model.state_dict(),
-    "optimizer_state_dict": optim.state_dict(),
-}
-
-torch.save(checkpoint, f"{trainer_cfg['name']}_e{n_epochs}.pth")
-writer.close()
+trainer = pl.Trainer(
+    max_epochs=20,
+    logger=logger,
+    accelerator=trainer_cfg["device"],
+    log_every_n_steps=10,
+    callbacks=[ImageReconstructionCallback(num_images=8)]
+)
+model = VQVAE(cfg)
+trainer.fit(model, datamodule=data_module)
