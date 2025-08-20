6db2f3a46e6234d4974b2ff7148891a123dcdb63
Abhyuday Tripathi <abhyuday964808@gmail.com>
2025-08-20 22:20:03 +0530

    remove perplexity

--- latent_diffusion/models/vqvae.py
+++ latent_diffusion/models/vqvae.py
@@ -82,8 +82,8 @@ class VQVAE(pl.LightningModule):
                 total_loss += codebook_loss
                 self.log("codebook_loss", codebook_loss, on_epoch=True, prog_bar=True, logger=True)
 
-            ppl, _ = measure_perplexity(encoding, self.codebook_size)
-            self.log("perplexity", ppl, on_epoch=True, logger=True)
+            # ppl, _ = measure_perplexity(encoding, self.codebook_size)
+            # self.log("perplexity", ppl, on_epoch=True, logger=True)
             self.log("commitment_loss", commitment_loss, on_epoch=True, prog_bar=True, logger=True)
 
         self.log("train_loss", total_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
