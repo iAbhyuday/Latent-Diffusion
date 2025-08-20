7212d9f49268a1f3742d2098171441361b10d397
Abhyuday Tripathi <abhyuday964808@gmail.com>
2025-08-20 22:10:41 +0530

    fix

--- latent_diffusion/modules/percept_loss.py
+++ latent_diffusion/modules/percept_loss.py
@@ -69,4 +69,4 @@ class PerceptualLoss(nn.Module):
             inp = layer(inp)
             x, y = inp.chunk(2)
             out.append(mse_loss(x, y))
-        return self.scale*pt.sum(out)
+        return self.scale*p.Tensor(out).sum()
